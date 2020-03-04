9b0472878a4730546c852d1a58d808427a7f6f6b
Wouter van Acht <woutervanacht@hotmail.com>
2020-03-04 22:32:21 +0100

    test production environment

--- wv8-finance-front-end/src/app/pages/dashboard/dashboard.component.ts
+++ wv8-finance-front-end/src/app/pages/dashboard/dashboard.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { NbMenuService } from '@nebular/theme';
-import { MenuItemsService } from '../../@core/utils/menu-items.service';
+import { Component, OnInit } from "@angular/core";
+import { NbMenuService } from "@nebular/theme";
+import { MenuItemsService } from "../../@core/utils/menu-items.service";
+import { environment } from "../../../environments/environment";
 
 @Component({
-  selector: 'ngx-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['dashboard.component.scss'],
+  selector: "ngx-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
+  env = environment;
 
-  constructor() {
-    
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 }
