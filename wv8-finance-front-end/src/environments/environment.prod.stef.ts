8327213ce08e54c5a89c46616f092dfbd277a493
Wouter van Acht <woutervanacht@hotmail.com>
2022-07-08 13:32:20 +0200

    Disable splitwise integration

--- wv8-finance-front-end/src/environments/environment.prod.stef.ts
+++ wv8-finance-front-end/src/environments/environment.prod.stef.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   base_url: "{secrets.BACKEND_URL}",
   public_url: "http://localhost:4200/#",
-  splitwiseIntegrationEnabled: false,
+  splitwiseIntegrationEnabled: true,
 };
